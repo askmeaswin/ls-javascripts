796420e453ad97329cf9c3987640577321c2e635
askmeaswin <68223918+askmeaswin@users.noreply.github.com>
2020-07-23 22:28:51 +0530

    Update Assignment3.js

--- Day5/Assignment3.js
+++ Day5/Assignment3.js
@@ -1,22 +1,5 @@
 console.log("Question 3")
 
-// fetch (' https://jsonplaceholder.typicode.com/todos/')
-
-
-// .then(response=>response.json())
-// .then((data=>console.log(data)))
-
-// function fetchTodo(){
-//     const response = fetch ('https://jsonplaceholder.typicode.com/todos/');
-//     const data = response;
-
-//     console.log (data);
-// }
-// fetchTodo();
-
-
-
-
 function fetchTodo(){
     fetch("https://jsonplaceholder.typicode.com/todos").then(response=>{console.log(response)
      return response.json()})
